10d06593c9e66a1fd4a1e019cf01292e0684550c
Sam Guard <sam.guard@outlook.com>
2022-04-23 12:42:50 +0100

    S;LGNDFPHRDG

--- src/App.js
+++ src/App.js
@@ -3,38 +3,50 @@ import { useRef, useState, useEffect } from "react";
 
 import Draggable from "react-draggable";
 function App() {
-  var multiplier = 1.777;
+	var multiplier = 1.777;
 
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
+	function getWindowDimensions() {
+		const { innerWidth: width, innerHeight: height } = window;
+		return {
+			width,
+			height,
+		};
+	}
 
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+	const [windowDimensions, setWindowDimensions] = useState(
+		getWindowDimensions()
+	);
 
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimensions(getWindowDimensions());
+		}
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+	const mapSize = 200;
 
-  return (
-    <div style={{height: windowDimensions.height * 0.9, width: windowDimensions.height * 0.9 / multiplier, border: "2px solid black", overflow: "hidden"}}>
-      <Draggable>
-      <div>
-        <Image imgName={"ground"} width={2000} heigh={2000}/>
-      </div>
-    </Draggable>
-    <SearchBar/>
-    </div>
-  )
+	return (
+		<div
+			style={{
+				height: windowDimensions.height * 0.9,
+				width: (windowDimensions.height * 0.9) / multiplier,
+				border: "2px solid black",
+			}}
+		>
+			<Draggable
+				bound={"parent"}
+        defaultPosition={{x:100, y:100}}
+			>
+				<div>
+					<Image imgName={"ground"} width={mapSize} heigh={mapSize} />
+				</div>
+			</Draggable>
+			<SearchBar />
+		</div>
+	);
 }
 
 export default App;
